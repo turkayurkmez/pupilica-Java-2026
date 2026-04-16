00b953db8643668ba1d18b6ed2690b2084d57445
türkay ürkmez <turkay.urkmez@outlook.com>
2026-04-16 21:26:15 +0300

    initalize repo

---
+++ ilkJavaUygulamasi/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+  //Her java uygulamasının bir başlangıç noktası vardır.
+  //JVM bir static void main başlangıç noktası arar.
+
+  public static void main(String[] args){
+      System.out.println("Merhaba Java Dünyası!");
+      System.out.print("bu satır");
+      System.out.print(" alt satıra");
+      System.out.println(" geçmiyor.");
+
+      System.out.println();
+
+//      System.out.println("Burası yeni bir satır");
+
+//Amacım java ile ekrana yazı yazmak.
+/*
+ Bu projenin amacı,
+ Java ile ilk adımlarımızı atmak
+ ve editörün tadını çıkartmak.....
+* */
+  System.out.println("Bir ifade her zaman (;) ile biter!");
+
+  //case-sensitive
+  System.out.println("Büyük-küçük harf duyarlı! ");
+  //"test" "TEST"
+  System.out.println("Kod blokları (scope) { } ile gruplandırılır ");
+
+  System.out.println("Sınıf adı ile dosya adı aynı olmak ZORUNDA!");
+
+  System.out.println("===========================================");
+  System.out.println("|                                         |");
+  System.out.println("|    JAVA    KATILIMCISI                  |");
+  System.out.println("|    Ad Soyad: Türkay Ürkmez              |");
+  System.out.println("|    Eposta: abc@xxx.com                  |");
+  System.out.println("|                                         |");
+  System.out.println("===========================================");
+
+  
+
+
+  }
+}
